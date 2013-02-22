c4b2bfec5d2f7dfdc13ea07fdbf652434bdfaaff
Wangwei <wei1.wang@dinglicom.com>
2013-02-22 10:46:02 +0800

    init.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+XFrame::Application.config.secret_token = '595c66bb5d906c1b062b753f7ee132c4afafc36850746ccbe5a4bc54456cd133aadf5aa41b2a0663abc053a6d8e084a049ea096f5df2bff6ddc7ec92890f8557'
